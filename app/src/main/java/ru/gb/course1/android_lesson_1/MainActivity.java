cc358736adf5fc6264d12754afe856a7cead29ea
Stepanav <vsa83@yandex.ru>
2021-12-15 12:02:57 +0300

    Не разобрался как менять цвета в календаре

--- app/src/main/java/ru/gb/course1/android_lesson_1/MainActivity.java
+++ app/src/main/java/ru/gb/course1/android_lesson_1/MainActivity.java
@@ -2,13 +2,35 @@ package ru.gb.course1.android_lesson_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    Button buttonSecondActivity;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSecondActivity = (Button) findViewById(R.id.buttonSecondActivity);
+        buttonSecondActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonSecondActivity:
+                Intent intent = new Intent(this, MainActivity2.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/ru/gb/course1/android_lesson_1/MainActivity2.java
@@ -0,0 +1,14 @@
+package ru.gb.course1.android_lesson_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends MainActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
